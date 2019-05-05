c3b9bbc326a2199fed41621d3528a9a5872f01e4
tonghs <tonghuashuai@gmail.com>
2019-05-05 06:39:32 +0000

    add ignore file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from ddns import *
+from ddns import DDNS
+
 
 def main():
-	ddns=DDNS("LOGIN_ID","LOGIN_TOKEN")
-	ddns.update()
+    ddns = DDNS("72982", "395fdff0dd5035f04734e9a679a9ece5")
+    ddns.update()
+
 
 if __name__ == '__main__':
-	main()
+    main()
